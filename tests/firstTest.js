aa4c4664beaff2fb0429da8d08eb4d25801dd925
Raja kumar prasad <ajarubab@gmail.com>
2025-08-09 12:31:26 +0530

    use of sleep function to delay the fast execution of code

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -4,8 +4,11 @@ const { Builder, Browser, By, Key, until } = require('selenium-webdriver')
   let driver = await new Builder().forBrowser(Browser.CHROME).build()
   try {
     await driver.get('https://www.google.com/ncr')
+    await driver.sleep(3000)
     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
+    await driver.sleep(30000)
     await driver.wait(until.titleIs('webdriver - Google Search'), 1000)
+    await driver.sleep(3000)
   } finally {
     await driver.quit()
   }
