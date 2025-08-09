c2312f7d038780a7529682d7f6de2f035530efe3
Raja kumar prasad <ajarubab@gmail.com>
2025-08-09 13:55:32 +0530

    generating automation test report using Mocha + Allure with java 8

--- README.md
+++ README.md
@@ -1,11 +1,38 @@
 Command to perform automation with javascript - selenium
 
+// initialise project
 1. npm init -y
+
+// install selenium
 2. npm i selenium-webdriver
+
+// create folder and files
 3. mkdir tests
 4. cd tests
 5. touch firstTest.js
 6. cd ..
 7. touch .gitignore
-8. To run the javascript automation script : 
-    node ./tests/firstTest.js
+
+// Run Selenium script without test framework (initially)
+8.    node ./tests/firstTest.js
+
+// To get the test report we need to run to Install Mocha & Allure dependencies
+9.    npm install --save-dev mocha mocha-allure-reporter allure-commandline
+
+10. To run the test javascript automation script : 
+    npm test
+
+11. Install Java 8 or higher ( needed for Allure CLI )
+
+12. Generate and view report :
+
+    allure generate allure-results --clean -o allure-report
+    allure open allure-report
+        
+        OR ( if script in package.json is modified then with addition of
+            "report": "allure generate allure-results --clean -o allure-report && allure open allure-report" in the script then )
+
+    Generate and view allure report just by
+        npm run report
+    
+    To exit from allure report web server session hit CTRL+C 
--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,15 +1,40 @@
-const { Builder, Browser, By, Key, until } = require('selenium-webdriver')
+const { Builder, Browser, By, Key, until } = require('selenium-webdriver');
+const { allure } = require('allure-mocha/runtime');
+const assert = require('assert');
 
-;(async function example() {
-  let driver = await new Builder().forBrowser(Browser.CHROME).build()
-  try {
-    await driver.get('https://www.google.com/ncr')
-    await driver.sleep(3000)
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
-    await driver.sleep(30000)
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000)
-    await driver.sleep(3000)
-  } finally {
-    await driver.quit()
-  }
-})()
+describe('Google Search Test', function () {
+    this.timeout(60000);
+
+    let driver;
+
+    before(async function () {
+        driver = await new Builder().forBrowser(Browser.CHROME).build();
+    });
+
+    after(async function () {
+        if (driver) {
+            await driver.quit();
+        }
+    });
+
+    it('should open Google and search for WebDriver', async function () {
+        try {
+            allure.logStep('Navigate to Google');
+            await driver.get('https://www.google.com/ncr');
+
+            allure.logStep('Type search term "webdriver"');
+            await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+
+            allure.logStep('Wait for title to contain "webdriver"');
+            await driver.wait(until.titleContains('webdriver'), 10000);
+
+            const title = await driver.getTitle();
+            assert.ok(title.toLowerCase().includes('webdriver'), 'Title does not contain "webdriver"');
+
+        } catch (error) {
+            const image = await driver.takeScreenshot();
+            allure.attachment('Screenshot on Failure', Buffer.from(image, 'base64'), 'image/png');
+            throw error;
+        }
+    });
+});
